8cda717db378eb42e9243ecff1be254b4e53304e
Zemian Deng <Zemian Deng@localhost>
2011-04-27 12:05:15 -0400

    Added quartz and spring scheduler config.

---
+++ src/main/java/myschedule/job/BeanJob.java
@@ -0,0 +1,17 @@
+package myschedule.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** BeanJob
+ *
+ * @author Zemian Deng
+ */
+public class BeanJob {
+	
+	private static Logger logger = LoggerFactory.getLogger(BeanJob.class);
+	
+	public void run() {
+		logger.info("Running bean job.");
+	}
+}
---
+++ src/main/java/myschedule/job/SimpleJob.java
@@ -0,0 +1,27 @@
+package myschedule.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** SimpleJob
+ *
+ * @author Zemian Deng
+ */
+public class SimpleJob implements Job {
+	
+	private static Logger logger = LoggerFactory.getLogger(SimpleJob.class);
+	
+	/**
+	 * Override @see org.quartz.Job#execute(org.quartz.JobExecutionContext) method.
+	 * @param context
+	 * @throws JobExecutionException
+	 */
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		logger.info("Job is running with context: " + context);
+	}
+
+}
