89b15cc3905cb42aa5ac1ff03fec08879f23638b
Zemian Deng <Zemian Deng@localhost>
2011-06-13 23:02:58 -0400

    Add sys prop to prevent auto startup/shutdown. Improved SchedulerService.

--- src/main/java/myschedule/service/SchedulerService.java
+++ src/main/java/myschedule/service/SchedulerService.java
@@ -12,15 +12,37 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerMetaData;
 import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Provide Quartz scheduling service to application.
- *
+ * 
+ * <p>Typically the {@link #scheduler} should be auto started and shutdown, but user may use 
+ * system property flag -Dmyschedule.quartz.autoStartAndShutdown=false to turn this off.
+ * (It's set by Spring's xml config file.)
+ * 
  * @author Zemian Deng
  */
-public class SchedulerService {
+public class SchedulerService implements InitializingBean, DisposableBean {
+
+	private static Logger logger = LoggerFactory.getLogger(SchedulerService.class);
 	
 	protected Scheduler scheduler;
+	
+	protected boolean autoStartAndShutdown = true;
+
+	private boolean waitForJobsToComplete = true;
+	
+	public void setAutoStartAndShutdown(boolean autoStartAndShutdown) {
+		this.autoStartAndShutdown = autoStartAndShutdown;
+	}
+	
+	public void setWaitForJobsToComplete(boolean waitForJobsToComplete) {
+		this.waitForJobsToComplete = waitForJobsToComplete;
+	}
 	
 	public void setScheduler(Scheduler scheduler) {
 		this.scheduler = scheduler;
@@ -28,7 +50,9 @@ public class SchedulerService {
 	
 	public Date scheduleJob(JobDetail jobDetail, Trigger trigger) {
 		try {
-			return scheduler.scheduleJob(jobDetail, trigger);
+			Date nextFireTime = scheduler.scheduleJob(jobDetail, trigger);
+			logger.info("Scheduled job=" + jobDetail.getFullName() + ", trigger=" + trigger.getFullName());
+			return nextFireTime;
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
 		}
@@ -40,6 +64,7 @@ public class SchedulerService {
 	public void scheduleJob(Trigger trigger) {
 		try {
 			scheduler.scheduleJob(trigger);
+			logger.info("Scheduled trigger=" + trigger);
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
 		}
@@ -79,8 +104,7 @@ public class SchedulerService {
 		}
 	}
 	
-	public JobDetail getJobDetail(String jobName, String jobGroup)
-	{	
+	public JobDetail getJobDetail(String jobName, String jobGroup) {	
 		try {
 			return scheduler.getJobDetail(jobName, jobGroup);
 		} catch (Exception e) {
@@ -88,8 +112,7 @@ public class SchedulerService {
 		}
 	}
 
-	public Trigger getTrigger(String triggerName, String triggerGroup)
-	{	
+	public Trigger getTrigger(String triggerName, String triggerGroup) {	
 		try {
 			return scheduler.getTrigger(triggerName, triggerGroup);
 		} catch (Exception e) {
@@ -101,8 +124,7 @@ public class SchedulerService {
 	 * Get a list of next fire time dates up to maxCount time. If next fire time needed
 	 * before maxCount, then there should be a null object in the last element of the list.
 	 */
-	public List<Date> getNextFireTimes(Trigger trigger, Date startTime, int maxCount)
-	{	
+	public List<Date> getNextFireTimes(Trigger trigger, Date startTime, int maxCount) {	
 		try {
 			List<Date> list = new ArrayList<Date>();
 			Date nextDate = startTime;
@@ -131,6 +153,7 @@ public class SchedulerService {
 			boolean success = scheduler.unscheduleJob(triggerName, triggerGroup);
 			if (!success)
 				throw new SchedulerException("Failed to unschedule job. Trigger name=" + triggerName + ", group=" + triggerGroup);
+			logger.info("Unscheduled job, triggerName=" + triggerName + ", triggerGroup=" + triggerGroup);
 			return trigger;
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
@@ -144,10 +167,12 @@ public class SchedulerService {
 	 */
 	public XmlJobLoader loadJobs(String xml) {
 		try {
-			XmlJobLoader xmlJobLoader = new XmlJobLoader();
+			XmlJobLoader xmlJobLoader = new XmlJobLoader(); // This is not only a loader, but also use to store what's loaded! 
 			String systemId = null;
 			InputStream istream = new ByteArrayInputStream(xml.getBytes());
 			xmlJobLoader.processStreamAndScheduleJobs(istream, systemId, scheduler);
+			logger.info("Xml job data loaded. Loaded jobs size=" + xmlJobLoader.getLoadedJobs().size() + 
+					" Loaded triggers size=" + xmlJobLoader.getLoadedTriggers().size());
 			return xmlJobLoader;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -157,6 +182,7 @@ public class SchedulerService {
 	public void standby() {
 		try {
 			scheduler.standby();
+			logger.info(scheduler.getSchedulerName() + " standby.");
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
 		}
@@ -165,6 +191,7 @@ public class SchedulerService {
 	public void start() {
 		try {
 			scheduler.start();
+			logger.info(scheduler.getSchedulerName() + " started.");
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
 		}
@@ -180,10 +207,38 @@ public class SchedulerService {
 			boolean success = scheduler.deleteJob(jobName, jobGroup);
 			if (!success)
 				throw new SchedulerException("Unable to delete jobName=" + jobName + ", jobGroup=" + jobGroup);
+			logger.info("Deleted jobName: " + jobName + ", jobGroup" + jobGroup + ". Also removed associated " + 
+					triggers.length + " triggers.");
 			return Arrays.asList(triggers);
 		} catch (SchedulerException e) {
 			throw new RuntimeException(e);
 		}
+	}	
+
+	protected String getSchedulerFullName() {
+		try {
+			return scheduler.getSchedulerName() + ":" + scheduler.getSchedulerInstanceId();
+		} catch (SchedulerException e) {
+			throw new RuntimeException(e);
+		}
+	}	
+
+	// 
+	// Spring bean lifecycle - init/destroy
+	// ====================================
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (autoStartAndShutdown) {
+			scheduler.start();
+			logger.info(getSchedulerFullName() + " auto started.");
+		}
 	}
-	
+
+	@Override
+	public void destroy() throws Exception {
+		if (autoStartAndShutdown) {
+			scheduler.shutdown(waitForJobsToComplete);
+			logger.info(getSchedulerFullName() + " auto shutdown with waitForJobsToComplete=" + waitForJobsToComplete);
+		}
+	}
 }
