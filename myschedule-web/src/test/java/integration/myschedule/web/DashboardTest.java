a2930d17b62214b2be5f0dbb3b905270cb974fed
Zemian Deng <Zemian Deng@localhost>
2011-10-26 23:34:08 -0400

    Moved webtest module into myschedule-web as integration tests.

--- myschedule-web/src/test/java/integration/myschedule/web/DashboardTest.java
+++ myschedule-web/src/test/java/integration/myschedule/web/DashboardTest.java
@@ -1,10 +1,7 @@
-package myschedule.webtest;
+package integration.myschedule.web;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import java.io.IOException;
-import java.io.InputStream;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -18,7 +15,6 @@ public class DashboardTest {
 		    assertThat(page.getTitleText(), is("MySchedule"));
 	
 		    String pageAsXml = page.asXml();
-		    //assertThat(pageAsXml, containsString(expectedHtml("myschedule/webtest/DashboardTest-list.html")));
 		    assertThat(pageAsXml, containsString("List of All Schedulers"));
 		    assertThat(pageAsXml, containsString("SCHEDULER NAME"));
 		    assertThat(pageAsXml, containsString("INITIALIZED"));
@@ -29,10 +25,5 @@ public class DashboardTest {
 		} finally {
 			webClient.closeAllWindows();
 		}
-	}
-
-	protected String expectedHtml(String resName) throws IOException {
-		InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resName);
-		return IOUtils.toString(inStream);
 	}
 }
