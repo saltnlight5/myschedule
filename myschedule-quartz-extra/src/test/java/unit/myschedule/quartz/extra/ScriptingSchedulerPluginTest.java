78014c9781de7bc64e4c839bee12b382fe1c1afc
Zemian Deng <Zemian Deng@localhost>
2011-10-29 03:21:28 -0400

    Added ScriptingSchedulerPluginTest with multiple script files.

--- myschedule-quartz-extra/src/test/java/unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest.java
+++ myschedule-quartz-extra/src/test/java/unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest.java
@@ -1,25 +1,33 @@
 package unit.myschedule.quartz.extra;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+
 import myschedule.quartz.extra.QuartzRuntimeException;
 import myschedule.quartz.extra.SchedulerTemplate;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.quartz.JobDetail;
+
 import unit.myschedule.quartz.extra.util.ResultFile;
 
 public class ScriptingSchedulerPluginTest {
-	public static ResultFile RESULT_FILE = new ResultFile("ScriptingSchedulerPluginTest.tmp");
+	public static ResultFile RESULT_FILE = new ResultFile(ScriptingSchedulerPluginTest.class.getSimpleName() + ".tmp");
 	
 	@Test
 	public void testScriptingSchedulerPlugin() throws Exception {
 		try {
 			// We will use the script to write to result file then verify
 			RESULT_FILE.resetFile();
-			SchedulerTemplate st = new SchedulerTemplate("unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz.properties");
+			SchedulerTemplate st = new SchedulerTemplate(
+					"unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz.properties");
 			st.startAndShutdown(700);
 			
 			List<String> result = RESULT_FILE.readLines();
@@ -37,7 +45,8 @@ public class ScriptingSchedulerPluginTest {
 	public void testScriptingSchedulerPluginFileNotFound() throws Exception {
 		try {
 			RESULT_FILE.resetFile();
-			SchedulerTemplate st = new SchedulerTemplate("unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz-filenotfound.properties");
+			SchedulerTemplate st = new SchedulerTemplate(
+					"unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz-filenotfound.properties");
 			st.startAndShutdown(700);
 			Assert.fail("We should faile with file not found.");
 		} catch (QuartzRuntimeException e) {
@@ -52,7 +61,8 @@ public class ScriptingSchedulerPluginTest {
 		try {
 			// We will use the script to write to result file then verify
 			RESULT_FILE.resetFile();
-			SchedulerTemplate st = new SchedulerTemplate("unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz-classpath.properties");
+			SchedulerTemplate st = new SchedulerTemplate(
+					"unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz-classpath.properties");
 			st.startAndShutdown(700);
 			
 			List<String> result = RESULT_FILE.readLines();
@@ -62,5 +72,17 @@ public class ScriptingSchedulerPluginTest {
 		} finally {
 			RESULT_FILE.delete();
 		}
+	}
+	
+	@Test
+	public void testScriptingSchedulerPluginMultiFiles() throws Exception {
+		SchedulerTemplate st = new SchedulerTemplate(
+				"unit/myschedule/quartz/extra/ScriptingSchedulerPluginTest-quartz-multifiles.properties");
+		List<JobDetail> jobs = st.getAllJobDetails();
+		assertThat(jobs.size(), is(2));
+		List<String> names = new ArrayList<String>();
+		names.add(jobs.get(0).getKey().getName());
+		names.add(jobs.get(1).getKey().getName());
+		assertThat(names, hasItems("hourlyJob1", "hourlyJob2"));
 	}
 }
