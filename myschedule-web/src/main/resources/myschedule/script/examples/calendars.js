20f19e9a114cbf4c8e123998ef380adec43cc67d
Zemian Deng <Zemian Deng@localhost>
2011-10-29 13:40:07 -0400

    Added calendars and simpleJobs.js examples

--- myschedule-web/src/main/resources/myschedule/script/examples/calendars.js
+++ myschedule-web/src/main/resources/myschedule/script/examples/calendars.js
@@ -1,8 +1,25 @@
-// Create a new job to the scheduler that runs every MON-FRI at 8am.
-importPackage(Packages.myschedule.quartz.extra.job);
-importPackage(Packages.java.util);
-var dataMap = new HashMap();
-dataMap.put('ScriptEngineName', 'JavaScript');
-dataMap.put('ScriptText', 'logger.info("It will take me a min to wake up."); sleep(60000);');
-var nextFireTime = scheduler.scheduleCronJob('MyDailyJob', '0 0 8 ? * MON-FRI', ScriptingJob, dataMap);
-webOut.println('MyDailyJob has been scheduled. Next fire time: ' + nextFireTime);
+//Create Quartz Calendar objects
+importPackage(Packages.org.quartz.impl.calendar);
+
+cal = CronCalendar('* * * * JAN ?');
+scheduler.addCalendar('SkipJan', cal, true, false);
+cal = CronCalendar('* * * ? * SAT,SUN');
+scheduler.addCalendar('SkipWeekEnd', cal, true, false);
+cal = CronCalendar('* * 12-13 * * ?');
+scheduler.addCalendar('SkipLunch', cal, true, false);
+
+// Create jobs that uses the calendars.
+job = scheduler.createJobDetail('CalJob', Packages.myschedule.quartz.extra.job.LoggerJob);
+trigger = scheduler.createCronTrigger('CalJob1', '0 0 * * * ?');
+trigger.setCalendarName('SkipWeekEnd');
+scheduler.scheduleJob(job, trigger);
+
+job = scheduler.createJobDetail('CalJob2', Packages.myschedule.quartz.extra.job.LoggerJob);
+trigger = scheduler.createCronTrigger('CalJob2', '0 0 * * * ?');
+trigger.setCalendarName('SkipJan');
+scheduler.scheduleJob(job, trigger);
+
+job = scheduler.createJobDetail('CalJob3',Packages. myschedule.quartz.extra.job.LoggerJob);
+trigger = scheduler.createCronTrigger('CalJob3', '0 0 * * * ?');
+trigger.setCalendarName('SkipLunch');
+scheduler.scheduleJob(job, trigger);
--- myschedule-web/src/main/resources/myschedule/script/examples/cronJobs.js
+++ myschedule-web/src/main/resources/myschedule/script/examples/cronJobs.js
@@ -1,8 +1,44 @@
-// Create a new job to the scheduler that runs every MON-FRI at 8am.
-importPackage(Packages.myschedule.quartz.extra.job);
-importPackage(Packages.java.util);
-var dataMap = new HashMap();
-dataMap.put('ScriptEngineName', 'JavaScript');
-dataMap.put('ScriptText', 'logger.info("It will take me a min to wake up."); sleep(60000);');
-var nextFireTime = scheduler.scheduleCronJob('MyDailyJob', '0 0 8 ? * MON-FRI', ScriptingJob, dataMap);
-webOut.println('MyDailyJob has been scheduled. Next fire time: ' + nextFireTime);
+// Using myschedule.quartz.extra.SchedulerTemplate API
+// ===================================================
+
+// For more on CRON expression format, see http://www.quartz-scheduler.org/api/2.1.0/org/quartz/CronExpression.html
+
+// Schedule hourly job on every MON-FRI
+scheduler.scheduleCronJob("hourlyCronJob", "0 0 * ? * MON-FRI", Packages.myschedule.quartz.extra.job.LoggerJob);
+
+// Schedule minutely job on JUN and DEC only
+scheduler.scheduleCronJob("minutelyCronJob", "0 * * * JUN,DEC ?", Packages.myschedule.quartz.extra.job.LoggerJob);
+
+// Schedule secondly job
+scheduler.scheduleCronJob("secondlyCronJob", "* * * * * ?", Packages.myschedule.quartz.extra.job.LoggerJob);
+
+// Schedule hourly job with job data and start time of 20s delay.
+scheduler.scheduleCronJob("hourlyCronJobWithStartTimeDelay", "0 0 * * * ?", Packages.myschedule.quartz.extra.job.LoggerJob, 
+		scheduler.mkMap('color', 'RED'), 
+		Packages.java.util.Date(Packages.java.lang.System.currentTimeMillis() + 20 * 1000));
+
+// Schedule one job with multiple triggers
+importPackage(Packages.org.quartz);
+job = scheduler.createJobDetail(JobKey.jobKey("jobWithMutltipleTriggers2"), Packages.myschedule.quartz.extra.job.LoggerJob, true, null);
+scheduler.addJob(job, false);
+trigger1 = scheduler.createCronTrigger("cronTrigger1", "0 0 * * * ?"); // hourly trigger
+trigger1.setJobKey(job.getKey());
+scheduler.scheduleJob(trigger1);
+trigger2 = scheduler.createCronTrigger("cronTrigger2", "0 * * * * ?"); // minutely trigger
+trigger2.setJobKey(job.getKey());
+scheduler.scheduleJob(trigger2);
+
+// Using Java org.scheduler.Scheduler API
+// ===================================================
+importPackage(Packages.org.quartz);
+quartzScheduler = scheduler.getScheduler();
+job = JobBuilder
+	.newJob(Packages.myschedule.quartz.extra.job.LoggerJob)
+	.withIdentity("hourlyCronJob2")
+	.build();
+trigger = TriggerBuilder
+	.newTrigger()
+	.withSchedule(
+		CronScheduleBuilder.cronSchedule("0 0 * * * ?"))
+	.build();
+quartzScheduler.scheduleJob(job, trigger);
