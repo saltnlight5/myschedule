7cbc97a6ba5d69fd9d25c76ebd14eba916841c85
Zemian Deng <Zemian Deng@localhost>
2011-11-16 17:29:29 -0500

    Renamed integration tests.

--- myschedule-web/src/test/java/integration/myschedule/web/page/StaticPagesTest.java
+++ myschedule-web/src/test/java/integration/myschedule/web/page/StaticPagesTest.java
@@ -4,25 +4,27 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class DashboardTest {
+public class StaticPagesTest {
 	private TestConfig testConfig = TestConfig.getInstance();
+	
 	@Test
 	public void testDashboardListPage() throws Exception {
 		WebClient webClient = new WebClient();
 		try {
 		    HtmlPage page = webClient.getPage(testConfig.getWebappUrl());
 		    assertThat(page.getTitleText(), is("MySchedule"));
-	
-		    String pageAsXml = page.asXml();
-		    assertThat(pageAsXml, containsString("List of All Schedulers"));
-		    assertThat(pageAsXml, containsString("SCHEDULER NAME"));
-		    assertThat(pageAsXml, containsString("INITIALIZED"));
-		    assertThat(pageAsXml, containsString("STARTED"));
-		    assertThat(pageAsXml, containsString("RUNNING SINCE"));
-		    assertThat(pageAsXml, containsString("# JOBS"));
-		    assertThat(pageAsXml, containsString("ACTION"));
+		    
+		    String pageText = page.asText();
+		    assertThat(pageText, containsString("This web application is powered by the following technologies and libraries."));
+		    assertThat(pageText, containsString("Java"));
+		    
+		    HtmlElement productInfo = page.getHtmlElementById("productInfo");
+		    assertThat(productInfo.asXml(), containsString("<a href=\"/myschedule/main/about\">"));
+		    assertThat(productInfo.asXml(), containsString("myschedule-"));
+		    assertThat(productInfo.asXml(), containsString("- Managing Quartz Schedulers with Ease."));
 		} finally {
 			webClient.closeAllWindows();
 		}
