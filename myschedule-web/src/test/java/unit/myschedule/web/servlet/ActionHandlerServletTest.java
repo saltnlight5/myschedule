8f67e35d7225e69f23e30d6a1f9155e2ec9d5266
Zemian Deng <Zemian Deng@localhost>
2011-11-16 11:20:33 -0500

    Added ViewData.findData tests. Added few more handler mapping tests.

--- myschedule-web/src/test/java/unit/myschedule/web/servlet/ActionHandlerServletTest.java
+++ myschedule-web/src/test/java/unit/myschedule/web/servlet/ActionHandlerServletTest.java
@@ -36,6 +36,29 @@ public class ActionHandlerServletTest {
 	}
 	
 	@Test
+	public void testHelpHandler() throws Exception {
+		ServletConfig config = mock(ServletConfig.class);
+		HttpServletResponse res = mock(HttpServletResponse.class);
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getMethod()).thenReturn("GET");
+		when(req.getRequestURI()).thenReturn("/main/help");
+		when(req.getContextPath()).thenReturn("");
+		when(req.getServletPath()).thenReturn("/main");
+		when(req.getQueryString()).thenReturn("");
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);		
+		when(req.getRequestDispatcher("/WEB-INF/jsp/main/help.jsp")).thenReturn(dispatcher);
+		
+		MyMainServlet main = new MyMainServlet();
+		main.init(config);
+		main.service(req, res);
+		main.destroy();
+
+		verify(dispatcher).forward(req, res);
+	}
+	
+	
+	@Test
 	public void testHelloHandler() throws Exception {
 		ServletConfig config = mock(ServletConfig.class);
 		HttpServletResponse res = mock(HttpServletResponse.class);
@@ -57,12 +80,82 @@ public class ActionHandlerServletTest {
 		verify(dispatcher).forward(req, res);
 	}
 	
+	@Test
+	public void testHello2Handler() throws Exception {
+		ServletConfig config = mock(ServletConfig.class);
+		HttpServletResponse res = mock(HttpServletResponse.class);
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getMethod()).thenReturn("GET");
+		when(req.getRequestURI()).thenReturn("/main/test/hello2");
+		when(req.getContextPath()).thenReturn("");
+		when(req.getServletPath()).thenReturn("/main");
+		when(req.getQueryString()).thenReturn("");
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);		
+		when(req.getRequestDispatcher("/WEB-INF/jsp/main/test/hello2.jsp")).thenReturn(dispatcher);
+		
+		MyMainServlet main = new MyMainServlet();
+		main.init(config);
+		main.service(req, res);
+		main.destroy();
+
+		verify(dispatcher).forward(req, res);
+	}
+
+	
+	@Test
+	public void testTest2Handler() throws Exception {
+		ServletConfig config = mock(ServletConfig.class);
+		HttpServletResponse res = mock(HttpServletResponse.class);
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getMethod()).thenReturn("GET");
+		when(req.getRequestURI()).thenReturn("/main/test2");
+		when(req.getContextPath()).thenReturn("");
+		when(req.getServletPath()).thenReturn("/main");
+		when(req.getQueryString()).thenReturn("");
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);		
+		when(req.getRequestDispatcher("/WEB-INF/jsp/main/test2.jsp")).thenReturn(dispatcher);
+		
+		MyMainServlet main = new MyMainServlet();
+		main.init(config);
+		main.service(req, res);
+		main.destroy();
+
+		verify(dispatcher).forward(req, res);
+	}
+	
+	@Test
+	public void testTest2HandlerWithSlash() throws Exception {
+		ServletConfig config = mock(ServletConfig.class);
+		HttpServletResponse res = mock(HttpServletResponse.class);
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getMethod()).thenReturn("GET");
+		when(req.getRequestURI()).thenReturn("/main/test2/");
+		when(req.getContextPath()).thenReturn("");
+		when(req.getServletPath()).thenReturn("/main");
+		when(req.getQueryString()).thenReturn("");
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);		
+		when(req.getRequestDispatcher("/WEB-INF/jsp/main/test2.jsp")).thenReturn(dispatcher);
+		
+		MyMainServlet main = new MyMainServlet();
+		main.init(config);
+		main.service(req, res);
+		main.destroy();
+
+		verify(dispatcher).forward(req, res);
+	}
+	
 	public static class MyMainServlet extends ActionHandlerServlet {
 		private static final long serialVersionUID = 1L;
 		@Override
 		public void init() {			
 			addActionHandler("/", new UrlRequestActionHandler());
+			addActionHandler("/help", new UrlRequestActionHandler());
 			addActionHandler("/test/hello", new UrlRequestActionHandler());
+			addActionHandler("/test/hello2", new UrlRequestActionHandler());
+			addActionHandler("/test2/", new UrlRequestActionHandler());
 		}
 	}
 }
--- myschedule-web/src/test/java/unit/myschedule/web/servlet/ViewDataTest.java
+++ myschedule-web/src/test/java/unit/myschedule/web/servlet/ViewDataTest.java
@@ -2,10 +2,17 @@ package unit.myschedule.web.servlet;
 
 import static myschedule.web.servlet.ViewData.mkMap;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import myschedule.web.servlet.ViewData;
 import org.junit.Assert;
 import org.junit.Test;
@@ -76,5 +83,114 @@ public class ViewDataTest {
 		} catch (IllegalArgumentException e) {
 			assertThat(e.getMessage(), containsString("Key must be a String type, but got: "));
 		}
+	}
+		
+	@Test
+	public void testFindDataBySession() throws Exception {
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		// Setup session data.
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute("num")).thenReturn(123);
+		when(session.getAttribute("letters")).thenReturn(new String[]{ "A", "B", "C" });
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getSession(false)).thenReturn(session);
+		
+		ViewData viewData = new ViewData("test", req, resp);
+		int num = viewData.findData("num");
+		assertThat(num, is(123));
+		String[] letters = viewData.findData("letters");
+		assertThat(letters, arrayContaining(new String[]{ "A", "B", "C" }));
+	}
+	
+	@Test
+	public void testFindDataByReqAttrs() throws Exception {
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		// Setup session data.
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute("num")).thenReturn(123);
+		when(session.getAttribute("letters")).thenReturn(new String[]{ "A", "B", "C" });
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getSession(false)).thenReturn(session);
+		// Setup request attributes data.
+		when(req.getAttribute("num")).thenReturn(456);
+		when(req.getAttribute("letters")).thenReturn(new String[]{ "D", "E", "F" });
+		
+		ViewData viewData = new ViewData("test", req, resp);
+		int num = viewData.findData("num");
+		assertThat(num, is(456));
+		String[] letters = viewData.findData("letters");
+		assertThat(letters, arrayContaining(new String[]{ "D", "E", "F" }));
+	}
+	
+	@Test
+	public void testFindDataByReqParams() throws Exception {
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		// Setup session data.
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute("num")).thenReturn(123);
+		when(session.getAttribute("letters")).thenReturn(new String[]{ "A", "B", "C" });
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getSession(false)).thenReturn(session);
+		// Setup request attributes data.
+		when(req.getAttribute("num")).thenReturn(456);
+		when(req.getAttribute("letters")).thenReturn(new String[]{ "D", "E", "F" });
+		// Setup request parameters data.
+		when(req.getParameterValues("num")).thenReturn(new String[]{ "789" });
+		when(req.getParameterValues("letters")).thenReturn(new String[]{ "G", "H", "I" });
+		
+		ViewData viewData = new ViewData("test", req, resp);
+		String num = viewData.findData("num");
+		assertThat(num, is("789"));
+		String[] letters = viewData.findData("letters");
+		assertThat(letters, arrayContaining(new String[]{ "G", "H", "I" }));
+	}
+
+	
+	@Test
+	public void testFindDataByDataMap() throws Exception {
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		// Setup session data.
+		HttpSession session = mock(HttpSession.class);
+		when(session.getAttribute("num")).thenReturn(123);
+		when(session.getAttribute("letters")).thenReturn(new String[]{ "A", "B", "C" });
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getSession(false)).thenReturn(session);
+		// Setup request attributes data.
+		when(req.getAttribute("num")).thenReturn(456);
+		when(req.getAttribute("letters")).thenReturn(new String[]{ "D", "E", "F" });
+		// Setup request parameters data.
+		when(req.getParameterValues("num")).thenReturn(new String[]{ "789" });
+		when(req.getParameterValues("letters")).thenReturn(new String[]{ "G", "H", "I" });
+		
+		ViewData viewData = new ViewData("test", req, resp);
+		// Setup dataMap
+		viewData.addData("num", 987, "letters", new String[]{ "J", "K", "L" });
+		
+		int num = viewData.findData("num");
+		assertThat(num, is(987));
+		String[] letters = viewData.findData("letters");
+		assertThat(letters, arrayContaining(new String[]{ "J", "K", "L" }));
+	}
+	
+	@Test
+	public void testFindDataNotFound() throws Exception {
+		try {
+			ViewData viewData = new ViewData("test");
+			viewData.addData("num", 987, "letters", new String[]{ "A", "B", "C" });
+			viewData.findData("test");
+			Assert.fail("We should throw exception when key not found.");
+		} catch (IllegalArgumentException e) {
+			assertThat(e.getMessage(), containsString("No data found using key: "));
+		}
+	}
+	
+	@Test
+	public void testFindDataWithDefaultValue() throws Exception {
+		ViewData viewData = new ViewData("test");
+		viewData.addData("num", 987, "letters", new String[]{ "A", "B", "C" });
+		String test = viewData.findData("test", "foo");
+		assertThat(test, is("foo"));
+		test = viewData.findData("test2", null);
+		assertThat(test, nullValue());
 	}
 }
