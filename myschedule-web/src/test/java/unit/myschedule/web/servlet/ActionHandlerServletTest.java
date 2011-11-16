c1cf8f150bed292fb9873087f2e4cce619217ea0
Zemian Deng <Zemian Deng@localhost>
2011-11-16 13:27:40 -0500

    Added action filter test.

--- myschedule-web/src/test/java/unit/myschedule/web/servlet/ActionHandlerServletTest.java
+++ myschedule-web/src/test/java/unit/myschedule/web/servlet/ActionHandlerServletTest.java
@@ -1,14 +1,20 @@
 package unit.myschedule.web.servlet;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import myschedule.web.servlet.ActionFilter;
 import myschedule.web.servlet.ActionHandlerServlet;
 import myschedule.web.servlet.UrlRequestActionHandler;
+import myschedule.web.servlet.ViewData;
 import org.junit.Test;
 
 public class ActionHandlerServletTest {
@@ -151,8 +157,34 @@ public class ActionHandlerServletTest {
 		verify(dispatcher).forward(req, resp);
 	}
 	
+	@Test
+	public void testActionFilter() throws Exception {
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		when(req.getMethod()).thenReturn("GET");
+		when(req.getRequestURI()).thenReturn("/main/test2/");
+		when(req.getContextPath()).thenReturn("");
+		when(req.getServletPath()).thenReturn("/main");
+		when(req.getQueryString()).thenReturn("");
+		
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);		
+		when(req.getRequestDispatcher("/WEB-INF/jsp/main/test2.jsp")).thenReturn(dispatcher);
+
+		ServletConfig config = mock(ServletConfig.class);
+		
+		MyMainServlet main = new MyMainServlet();
+		main.init(config);
+		main.service(req, resp);
+		main.destroy();
+
+		verify(dispatcher).forward(req, resp);
+		assertThat(MyMainServlet.testFilter.beforeActionList.size(), is(1));
+		assertThat(MyMainServlet.testFilter.afterActionList.size(), is(1));
+	}
+	
 	public static class MyMainServlet extends ActionHandlerServlet {
 		private static final long serialVersionUID = 1L;
+		public static MyFilter testFilter = new MyFilter();
 		@Override
 		public void init() {			
 			addActionHandler("/", new UrlRequestActionHandler());
@@ -160,6 +192,26 @@ public class ActionHandlerServletTest {
 			addActionHandler("/test/hello", new UrlRequestActionHandler());
 			addActionHandler("/test/hello2", new UrlRequestActionHandler());
 			addActionHandler("/test2/", new UrlRequestActionHandler());
+			
+			addActionFilter("/test", testFilter);
 		}
+	}
+	
+	public static class MyFilter implements ActionFilter {
+		public List<String> beforeActionList = new ArrayList<String>();
+		public List<String> afterActionList = new ArrayList<String>();
+		
+		@Override
+		public ViewData beforeAction(String actionPath, HttpServletRequest req, HttpServletResponse resp)
+				throws Exception {
+			beforeActionList.add(actionPath);
+			return null;
+		}
+
+		@Override
+		public void afterAction(ViewData viewData, String actionPath, HttpServletRequest req, HttpServletResponse resp)
+				throws Exception {
+			afterActionList.add(viewData.getViewName());
+		}		
 	}
 }
