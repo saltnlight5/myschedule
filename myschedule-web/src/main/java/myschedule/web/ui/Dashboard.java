cd402cb339b5698bb18c09abddb6af8ecec0ee66
Zemian Deng <Zemian Deng@localhost>
2013-02-23 22:31:19 -0500

    Renamed UI classes for consistency and match the screen name.

---
+++ myschedule-web/src/main/java/myschedule/web/ui/Dashboard.java
@@ -0,0 +1,17 @@
+package myschedule.web.ui;
+
+import com.vaadin.ui.Panel;
+
+/**
+ * UI screen for the schedulers dashboard. This is the default main screen for the MySchedule application.
+ *
+ * User should see all the configured schedulers in this screen with their status and actions. The screen
+ * should also have a toolbar to allow add new scheduler config entry.
+ */
+public class Dashboard extends Panel {
+	private static final long serialVersionUID = 1L;
+
+	public Dashboard() {
+
+	}
+}
