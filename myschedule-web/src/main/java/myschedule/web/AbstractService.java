18c6e97c17d76557dd92ea4d93c6fef0fd4c850b
Zemian Deng <Zemian Deng@localhost>
2013-03-03 20:16:45 -0500

    Refactored and added AbstractService to the application for reuse.

---
+++ myschedule-web/src/main/java/myschedule/web/AbstractService.java
@@ -0,0 +1,29 @@
+package myschedule.web;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author Zemian Deng
+ */
+public abstract class AbstractService implements Service {
+    private AtomicBoolean inited = new AtomicBoolean(false);
+
+    @Override
+    public void init() {
+        if (inited.get())
+            return;
+        initService();
+        inited.set(true);
+    }
+
+    @Override
+    public void destroy() {
+        if (!inited.get())
+            return;
+        destroyService();
+        inited.set(false);
+    }
+
+    public abstract void initService();
+    public abstract void destroyService();
+}
