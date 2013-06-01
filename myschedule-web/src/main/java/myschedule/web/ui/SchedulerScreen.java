ea6670aa6bee3fb5e36c19645d8a9f70196de1a2
zemian.deng <zemian.deng@localhost>
2013-06-01 13:39:02 -0400

    Added TabSheet to switch scheduler screen content.

--- myschedule-web/src/main/java/myschedule/web/ui/SchedulerScreen.java
+++ myschedule-web/src/main/java/myschedule/web/ui/SchedulerScreen.java
@@ -2,10 +2,7 @@ package myschedule.web.ui;
 
 import com.vaadin.data.Property;
 import com.vaadin.event.ItemClickEvent;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import myschedule.quartz.extra.SchedulerTemplate;
 import myschedule.web.MySchedule;
 import org.apache.commons.lang.StringUtils;
@@ -32,24 +29,20 @@ import java.util.List;
 public class SchedulerScreen extends VerticalLayout {
     private static final Logger LOGGER = LoggerFactory.getLogger(SchedulerScreen.class);
     private static final long serialVersionUID = 1L;
+    private MySchedule mySchedule = MySchedule.getInstance();
     private MyScheduleUi myScheduleUi;
     private String schedulerSettingsName;
-    private MySchedule mySchedule = MySchedule.getInstance();
-    private HorizontalLayout schedulerToolbar;
-    private VerticalLayout content;
+    private SchedulerContent content;
 
     public SchedulerScreen(MyScheduleUi myScheduleUi, String schedulerSettingsName) {
         this.myScheduleUi = myScheduleUi;
         this.schedulerSettingsName = schedulerSettingsName;
-        initSchedulerToolbar();
         initContent();
     }
 
     private void initContent() {
-        content = new VerticalLayout();
+        content = new SchedulerContent();
         addComponent(content);
-
-        switchContentToJobsWithTriggers();
     }
 
     private void switchContentToJobsWithTriggers() {
@@ -62,33 +55,12 @@ public class SchedulerScreen extends VerticalLayout {
         content.addComponent(new JobsWithoutTriggersContent());
     }
 
-
-    private void initSchedulerToolbar() {
-        schedulerToolbar = new HorizontalLayout();
-        addComponent(schedulerToolbar);
-
-        schedulerToolbar.addComponent(createScriptConsoleButton());
-        schedulerToolbar.addComponent(createJobsWithoutTriggersButton());
-    }
-
     private Button createJobsWithoutTriggersButton() {
         Button button = new Button("Jobs Without Triggers");
         button.addClickListener(new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent event) {
                 switchContentToJobsWithoutTriggers();
-            }
-        });
-        return button;
-    }
-
-    private Button createScriptConsoleButton() {
-        Button button = new Button("ScriptConsole");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                ScriptConsoleWindow console = new ScriptConsoleWindow(myScheduleUi, schedulerSettingsName);
-                myScheduleUi.addWindow(console);
             }
         });
         return button;
@@ -231,5 +203,44 @@ public class SchedulerScreen extends VerticalLayout {
 
     class JobsWithoutTriggersContent extends VerticalLayout {
 
+    }
+
+    class SchedulerContent extends VerticalLayout {
+        TabSheet tabSheet;
+        VerticalLayout jobsWithTriggersContent = new VerticalLayout();
+        VerticalLayout jobsWithoutTriggersContent = new VerticalLayout();
+        VerticalLayout scriptConsoleContent = new VerticalLayout();
+        public SchedulerContent() {
+            tabSheet = new TabSheet();
+            addComponent(tabSheet);
+
+            tabSheet.addTab(jobsWithTriggersContent, "Jobs with Triggers");
+            tabSheet.addTab(jobsWithoutTriggersContent, "Jobs without Triggers");
+            tabSheet.addTab(scriptConsoleContent, "Script Console");
+
+            tabSheet.addSelectedTabChangeListener(new TabSheet.SelectedTabChangeListener() {
+                @Override
+                public void selectedTabChange(TabSheet.SelectedTabChangeEvent selectedTabChangeEvent) {
+                    TabSheet tabSheet = selectedTabChangeEvent.getTabSheet();
+                    VerticalLayout selectedContent = (VerticalLayout)tabSheet.getSelectedTab();
+                    if (selectedContent == jobsWithTriggersContent) {
+                        jobsWithTriggersContent.removeAllComponents();
+                        jobsWithTriggersContent.addComponent(new JobsWithTriggersContent());
+                    } else if (selectedContent == jobsWithoutTriggersContent) {
+                        jobsWithoutTriggersContent.removeAllComponents();
+                        jobsWithoutTriggersContent.addComponent(new JobsWithoutTriggersContent());
+                    } else if (selectedContent == scriptConsoleContent) {
+                        ScriptConsoleWindow console = new ScriptConsoleWindow(myScheduleUi, schedulerSettingsName);
+                        myScheduleUi.addWindow(console);
+                        tabSheet.setSelectedTab(0);
+                    }
+                }
+            });
+
+            // default trigger first tab selection.
+            tabSheet.setSelectedTab(jobsWithTriggersContent);
+            jobsWithTriggersContent.removeAllComponents();
+            jobsWithTriggersContent.addComponent(new JobsWithTriggersContent());
+        }
     }
 }
