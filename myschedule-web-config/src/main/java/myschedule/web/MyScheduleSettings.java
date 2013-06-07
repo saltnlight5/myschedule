f035c45395deca7afc67816fc4f90fb50896895d
zemian.deng <zemian.deng@localhost>
2013-06-07 15:40:37 -0400

    Improve loading myschedule-settings.properties file.

--- myschedule-web-config/src/main/java/myschedule/web/MyScheduleSettings.java
+++ myschedule-web-config/src/main/java/myschedule/web/MyScheduleSettings.java
@@ -11,6 +11,9 @@ import org.slf4j.LoggerFactory;
  * values and their usage description. All settings should have key prefixed with SETTINGS_KEY_PREFIX.
  * 
  * User may use Java system properties "-Dmyschedule.settings=my.propertis" to override any settings.
+ * <p>
+ * Or user may alternatively use Env variable "MYSCHEDULE_SETTINGS" to specify the property file.
+ * </p>
  * 
  * @author zemian
  *
@@ -28,23 +31,37 @@ public class MyScheduleSettings {
 	}
 	
 	private void initProps() {
+        LOGGER.debug("Sys props " + SETTINGS_SYS_PROPS_KEY + "=" + System.getProperty(SETTINGS_SYS_PROPS_KEY));
+        LOGGER.debug("Sys env " + SETTINGS_SYS_ENV_KEY + "=" + System.getenv(SETTINGS_SYS_ENV_KEY));
+
 		// Load default settings first
 		LOGGER.debug("Loading default settings {}", DEFAULT_SETTINGS_URL);
-		props = new Props(DEFAULT_SETTINGS_URL);
+		props = new Props();
+        props.load(DEFAULT_SETTINGS_URL); // Don't use the constructor to avoid early vars expansion.
 		
 		// Load custom settings properties
-		String customSettings = System.getProperty(SETTINGS_SYS_PROPS_KEY, null);
+		String customSettings = System.getProperty(SETTINGS_SYS_PROPS_KEY);
 		if (customSettings == null)
 			customSettings = System.getenv(SETTINGS_SYS_ENV_KEY);
-		
+
+        // -- Auto detect OpenShift env (a workaround to their system that they failed to set env through hook.)
+        if (customSettings == null && System.getenv("OPENSHIFT_DATA_DIR") != null)
+            customSettings = System.getenv("OPENSHIFT_DATA_DIR") + "/myschedule-settings.properties";
+
+        // -- Load it
 		if (customSettings != null) {
-			LOGGER.info("Loadding custom settings {}", customSettings);
+			LOGGER.debug("Loadding additional custom settings {}", customSettings);
 			props.load(customSettings);
 		}
 		
 		// Expand vars if there are any.
 		props.expandVariables();
-		LOGGER.debug("MySchedule settings are ready.");
+		LOGGER.info("MySchedule settings are ready. myschedule.web.dataStoreDir=" + props.getString("myschedule.web.dataStoreDir"));
+
+        // Enable debug output if it's on.
+        if (LOGGER.isDebugEnabled())
+            for (String key : props.keySet())
+                LOGGER.debug("Settings value " + key + "=" + props.get(key));
 	}
 
 	public long getPauseTimeAfterShutdown() {
