a25323d9f1a108015ab178606d42635db0c47684
zemian.deng <zemian.deng@localhost>
2013-06-08 15:40:29 -0400

    Fixed scheduler config doc points to 1.8.6. Updated JS examples.

--- myschedule-web-config/src/main/resources/myschedule/web/templates/scripts/advanceJobs.js
+++ myschedule-web-config/src/main/resources/myschedule/web/templates/scripts/advanceJobs.js
@@ -1,3 +1,25 @@
 // JavaScript Examples
 
-// No examples available for Quartz-1.8.x version.
+// Create some durable jobs without triggers.
+importClass(Packages.myschedule.quartz.extra.job.LoggerJob);
+importPackage(Packages.org.quartz);
+job = scheduler.createJobDetail("durableJob1", "DEFAULT", LoggerJob, true, null);
+scheduler.addJob(job, false);
+
+job = scheduler.createJobDetail("durableJob2", "DEFAULT", LoggerJob, true, null);
+scheduler.addJob(job, false);
+
+job = scheduler.createJobDetail("durableJob3", "DEFAULT", LoggerJob, true, null);
+scheduler.addJob(job, false);
+
+
+// Create a job that takes 15 secs to run.
+dataMap = HashMap();
+dataMap.put('ScriptEngineName', 'JavaScript');
+dataMap.put('ScriptText',
+		'logger.info("I take 15 secs to run...");\n' +
+		'Packages.java.lang.Thread.sleep(15000);\n' +
+		'logger.info("I am done.");\n');
+scheduler.scheduleSimpleJob("15secJob", -1, 60 * 60 * 1000, ScriptingJob,
+		dataMap,
+		Packages.java.util.Date(System.currentTimeMillis() + 20 * 1000));
